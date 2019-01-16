4169a953a22b7bc79dac601ff90a146e2ba3f06a
Marcos <Rojasaularmarcos@gmail.com>
2019-01-16 22:00:20 +0100

    Inputs with labels
    Labels a los inputs para identificar

--- resources/views/Usuarios.blade.php
+++ resources/views/Usuarios.blade.php
@@ -12,26 +12,47 @@
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-5">
-					<h2>Registro de Usuarios</h2>
-
+					<h1>Registro de Usuarios</h1>
 						<form action="" method="POST">
-							<div class="form-group">
-								<input type="text" name="usuario" class="form-control" required placeholder="Usuario">
+							
+							<div class="form-group row">
+								<label for="usuario" class="col-sm-2 col-form-label">Usuario</label>
+									<div class="col-sm-10">
+										<input type="text" name="usuario" class="form-control" id="usuario" placeholder="Usuario" maxlength="16" required >
+									</div>
 							</div>
 
-							<form>
-								<div class="input-group">
-  									<input type="text" class="form-control"  required placeholder="Nombre">
-  									<input type="text" class="form-control" required placeholder="Apellidos">
+  								<div class="form-group row">
+    								<label for="nombre" class="col-sm-2 col-form-label">Nombre</label>
+    								<div class="col-sm-10">
+      									<input type="text" class="form-control" id="nombre" placeholder="Nombre" maxlength="20" required>
+    								</div>
   								</div>
-  							</form>
 
-							<div class="form-group">
-								<input type="tel" name="telefono" class="form-control" required placeholder="Telefono" maxlength="9">
+								<div class="form-group row">
+    								<label for="Apellidos" class="col-sm-2 col-form-label">Apellidos</label>
+    								<div class="col-sm-10">
+      									<input type="text" class="form-control" id="Apellidos" placeholder="Apellidos" maxlength="20" required>
+    								</div>
+  								</div>
+  							
+
+							<div class="form-group row">
+								<label for="telefono" class="col-sm-2 col-form-label">Telefono</label>
+								<div class="col-sm-10">	
+								<input type="tel" name="telefono" class="form-control" id="telefono" placeholder="Telefono" maxlength="9" required>
+								</div>
 							</div>
-							<div class="form-group">
-								<input type="email" name="email" class="form-control" pattern=".+@gmail.com" required placeholder="Usuario@gmail.com" title="Solo valido gmail">
+
+
+							<div class="form-group row">
+								<label for="email" class="col-sm-2 col-form-label">Email</label>
+								<div class="col-sm-10">
+								<input type="email" name="email" class="form-control" id="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"  placeholder="Usuario@example.com" required >
+								</div>
 							</div>
+
+
 							<div class="form-group">
 								<input type="submit" name="Registrarse" class="btn btn-primary">
 							</div>
